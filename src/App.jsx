f63016aeab6b9cf400d634ba41826c7e4b3c8151
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-09-20 11:15:53 +0200

    added event listener onClick

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,22 @@ export default function App(){
     setNewItem("")
   }
 
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo=> {
+        if (todo.id === id) {
+          return{...todo,completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos =>{
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
   return(
     <>
      <form onSubmit={handleSubmit} className="new-item-form">
@@ -35,11 +51,13 @@ export default function App(){
       return(
       <li key={todo.id}>
       <label>
-        <input type="checkbox" checked={todo.completed} />
+        <input type="checkbox" checked={todo.completed} 
+        onChange={e => toggleTodo(todo.id, e.target.checked)}/>
         {todo.title}
         item1
       </label>
-      <button className="btn btn-danger">Delete</button>)
+      <button onClick={() => deleteTodo(todo.id)}
+      className="btn btn-danger">Delete</button>
       </li>
       )
     })}
