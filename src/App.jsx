c78a7d363ba86b442e0a2d1be16146e7bec80b73
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-09-21 11:10:06 +0200

    Added two more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,59 @@
-import { useState } from "react"
+import {useEffect, useState} from "react"
+import {NewTodoForm} from "./NewTodoForm";
 import "./style.css"
+import {TodoList} from "./TodoList";
+
 export default function App(){
-  const [newItem, setNewItem] = useState("item 1")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStprage.getItem("ITEMS")
+    if (localValue == null) return }[]
+  return JSON.parse(localValue)
+})
 
-  function handleSubmit(e){
-    e.preventDefault()
+  useEffect(() => {
+    localStorage.setItem("ITEM",JSON.stringify(todos))
+  }, [todos])
 
-    setTodos(currentTodos => 
-      [
-        ...currentTodos,
-        { id: crypto.randomUUID(),title: newItem,completed: false}
-      ]
-    );
-    setNewItem("")
-  }
-
-  function toggleTodo(id, completed){
-    setTodos(currentTodos => {
-      return currentTodos.map(todo=> {
-        if (todo.id === id) {
-          return{...todo,completed}
-        }
-        return todo
+  function addTodo(title){
+    setTodos(title){
+      setTodos(currentTodos => {
+        return[
+          ...currentTodos,
+          {
+            id:crypto.randomUUID(),title,completed:false
+          },
+        ]
       })
-    })
+    }
+
+    function toggleTodo(id, completed){
+      setTodos(currentTodos =>{
+        return currentTodos.map(todo) => {
+          if (todo.id === id){
+            return {...todo,completed}
+          }
+          return todo
+        }
+      })
+    }
+    }
+
   }
 
-  function deleteTodo(id) {
-    setTodos(currentTodos =>{
-      return currentTodos.filter(todo => todo.id !== id)
-    })
-  }
+
+
+ function deleteTodo(id){
+  setTodos(currentTodos =>{
+    return currentTodos.filter(todo => todo,id !== id)
+  })
+
+
+  
   return(
-    <>
-     <form onSubmit={handleSubmit} className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input value={newItem} 
-      onChange={e => setNewItem(e.target.value)}
-      type="text" 
-      id="item" />
-    </div>
-    <button className="btn">Add</button>
-
-  </form>
+  <>
+  <NewTodoForm onSubmit={addTodo}/>
   <h1 className="header">Todo List</h1>
-  <ul className="list">
-    {todos.length === 0 && "No Todos"}
-    {todos.map(todo => {
-      return(
-      <li key={todo.id}>
-      <label>
-        <input type="checkbox" checked={todo.completed} 
-        onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-        {todo.title}
-        item1
-      </label>
-      <button onClick={() => deleteTodo(todo.id)}
-      className="btn btn-danger">Delete</button>
-      </li>
-      )
-    })}
-   
-  </ul>
+ <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
   </>
   )
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,23 @@
+export function TodoList({todos,toggleTodo,deleteTodo}){
+    return(
+    <ul className="list">
+    {todos.length === 0 && "No Todos"}
+    {todos.map(todo => {
+      return(
+      <li key={todo.id}>
+      <label>
+        <input type="checkbox"
+         checked={todo.completed} 
+        onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+        {todo.title}
+        item1
+      </label>
+      <button onClick={() => deleteTodo(todo.id)}
+      className="btn btn-danger">Delete</button>
+      </li>
+      )
+    })}
+   
+  </ul>
+    )
+}
