f23c0d61c441b6f65e7e6227544f7c395b4d4ebd
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-09-25 07:47:20 +0200

    Added delete button

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,23 +1,16 @@
-export function TodoList({todos,toggleTodo,deleteTodo}){
-    return(
+export function TodoList({ todos}) {
+  return (
     <ul className="list">
-    {todos.length === 0 && "No Todos"}
-    {todos.map(todo => {
-      return(
-      <li key={todo.id}>
-      <label>
-        <input type="checkbox"
-         checked={todo.completed} 
-        onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-        {todo.title}
-        item1
-      </label>
-      <button onClick={() => deleteTodo(todo.id)}
-      className="btn btn-danger">Delete</button>
-      </li>
-      )
-    })}
-   
-  </ul>
-    )
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo =>{
+        
+        return (
+          <TodoItem 
+          {...todo}
+          key={todo.id}
+          />
+        
+})}
+    </ul>
+  );
 }
